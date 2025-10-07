64fb7b887f5f84c7b0ffb252d72b4f85710ea519
Denis Miller <millerdenis@gmail.com>
2025-10-07 22:27:28 +0200

    console activity source

--- src/Avatar.Client.Console/Program.cs
+++ src/Avatar.Client.Console/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+using var traceProvider = OpenTelemetry.Sdk.CreateTracerProviderBuilder()
+    .SetResourceBuilder(
+        ResourceBuilder.CreateDefault()
+            .AddService("ConsoleTool")
+    )
+    .AddSource(ApplicationDiagnostics.ActivitySourceName)
+    .AddConsoleExporter()
+    .Build();
+
+
+using var activity = ApplicationDiagnostics.ActivitySource.StartActivity("Do Work");
 Console.WriteLine("Hello, World!");
+
+
+public static class ApplicationDiagnostics
+{
+    public static readonly ActivitySource ActivitySource = new(ActivitySourceName);
+    public const string ActivitySourceName = "Console.Tool.Diagnostics";
+}
+
+
+
+
+
+
