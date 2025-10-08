aede578e2dcdadfca6f8a6ed001191e08965d0f4
Denis Miller <millerdenis@gmail.com>
2025-10-08 20:13:31 +0200

    podmain

--- docker/podman_start.sh
+++ docker/podman_start.sh
@@ -2,9 +2,13 @@
 
 # sudo dnf install -y podman-compose
 
+podman-compose -f podman_compose.yaml down
 podman-compose -f podman_compose.yaml up -d
 
 podman ps -a
 
+# Postgres Admin
 xdg-open "http://127.0.0.1:5050" >/dev/null 2>&1 &
 
+# Jaeger UI
+xdg-open "http://127.0.0.1:16686" >/dev/null 2>&1 &
--- src/Avatar.Client.Console/Program.cs
+++ src/Avatar.Client.Console/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
 using var traceProvider = OpenTelemetry.Sdk.CreateTracerProviderBuilder()
     .SetResourceBuilder(
         ResourceBuilder.CreateDefault()
-            .AddService("ConsoleTool")
+            .AddService("Avatar.Client.Console")
+            .AddAttributes([
+                new KeyValuePair<string, object>("service.version", Assembly.GetExecutingAssembly().GetName().Version!.ToString())
+            ])
     )
     .AddSource(ApplicationDiagnostics.ActivitySourceName)
     .AddConsoleExporter()
     .Build();
-
 
 using var activity = ApplicationDiagnostics.ActivitySource.StartActivity("Do Work");
 Console.WriteLine("Hello, World!");
